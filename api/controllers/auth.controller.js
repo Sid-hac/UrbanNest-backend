aaaa9be552f717706c1ee820e1d404a575b9449c
sid <sidengineer.014@gmail.com>
2024-06-20 12:35:59 +0530

    secure = true for using of samesite

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -60,7 +60,7 @@ export const login = async (req, res) => {
     res.cookie("token", token , {
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true,
-        // secure: true,
+        secure: true,
         sameSite: "none",
       })
       .status(200)
